2bb7a57a539889e028c17eff07d5cc778df34ba8
Dmitry Frolkov <dmitfrol152@mail.ru>
2025-11-13 11:41:50 +0300

    mvp job tracker: avatar/logo is empty fixed

--- MVP/job-tracker/src/shared/ui/LogoUi/Logo.tsx
+++ MVP/job-tracker/src/shared/ui/LogoUi/Logo.tsx
@@ -1,12 +1,17 @@
 import LogoIcon from "@shared/assets/svg/logo.svg?react";
+import emptyAvatar from "@shared/assets/png/avatar-empty.png";
 import type { LogoProps } from "./types";
 
 export function Logo({ profileSrc, user }: LogoProps) {
   return (
     <div className="flex items-center gap-3">
-      {user ? (
+      {user && profileSrc !== "" ? (
         <div className="flex flex-col h-10 w-10 overflow-hidden justify-center items-center rounded-full border border-[var(--color-green-600)]">
           <img src={profileSrc} alt="Avatar" />
+        </div>
+      ) : user && profileSrc === "" ? (
+        <div className="flex flex-col h-10 w-10 overflow-hidden justify-center items-center rounded-full border border-[var(--color-green-600)]">
+          <img src={emptyAvatar} alt="Avatar" />
         </div>
       ) : (
         <LogoIcon className="w-10" />
